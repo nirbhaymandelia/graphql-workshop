9186b11032f85dfc75e9f91f7d545c30d2dd9630
Saravana Gopal <sgopal@sapient.com>
2020-06-13 14:04:43 +0530

    created service for customer

---
+++ customers/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const customerRoutes = require('./routes');
+const mongoose = require('mongoose');
+
+const port = process.env.CUSTOMER_SERVICE_PORT || 3010;
+const url = process.env.USER_DB_URL || 'mongodb://localhost:27017/workshop-customer-db';
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err)=> {
+    if(err){
+        console.error('---Error connecting to customer db');
+    }else{
+        console.log('---Successfully connected to customer db');
+    }
+})
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/', customerRoutes);
+
+app.listen(port, () => {
+    console.log(`Customer service is listening on port ${port}`);
+});
---
+++ customers/models/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+    name: String,
+    email: {type: String,required: true, unique: true},
+    password: String
+})
+
+CustomerSchema.pre('save', async function(next){
+    const hashPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashPassword;
+    next();
+})
+
+CustomerSchema.methods.isValidPassword = async function(newPassword){
+    const match = await bcrypt.compare(newPassword, this.password);
+    return match;
+}
+
+CustomerSchema.virtual('id').get(function(){
+    return this._id.toHexString();
+})
+
+CustomerSchema.set('toJSON', {
+    virtuals: true
+})
+
+const Customer = mongoose.model('customers', CustomerSchema);
+
+module.exports = Customer;
