d2bbf1f2b6fdbff860ad6d0b24a6b85a892aab68
Manish Dhar <dwivem2@medtronic.com>
2020-06-13 13:40:27 +0530

    adding customer-graphql-part

--- graphql-server/schema/index.js
+++ graphql-server/schema/index.js
@@ -1,35 +1,49 @@
-const {gql} = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-    type Query {
-        users: [User]
-    }
+  type Query {
+    users: [User]
+    customers: [Customer]
+  }
 
-    type Mutation {
-        registerUser(user: UserInput): User,
-        login(user: LoginInput): String
-    }
+  type Mutation {
+    registerUser(user: UserInput): User
+    login(user: LoginInput): String
+    registerCustomer(customer: CustomerInput): Customer
+  }
 
-    
+  input UserInput {
+    name: String
+    age: Int
+    password: String
+    email: String
+  }
 
-    input UserInput {
-        name: String
-        age: Int
-        password: String
-        email: String
-    }
+  input LoginInput {
+    email: String
+    password: String
+  }
 
-    input LoginInput {
-        email: String
-        password: String
-    }
+  input CustomerInput {
+    name: String
+    email: String
+    password: String
+    address: String
+  }
 
-    type User {
-        id: ID
-        name: String
-        email: String
-        age: Int
-    }
+  type User {
+    id: ID
+    name: String
+    email: String
+    age: Int
+  }
+
+  type Customer {
+    id: ID
+    name: String
+    email: String
+    address: String
+  }
 `;
 
-module.exports =typeDefs;
+module.exports = typeDefs;
